a0a7aeac824a7d4b336a3c66426528ed8a2827e5
joric <joric@users.noreply.github.com>
2025-05-20 01:41:50 +0500

    move svg to css

---
+++ resources/svg_export_inline.py
@@ -0,0 +1,10 @@
+from xml.dom.minidom import parse, parseString
+
+doc = parse('icons.svg')
+f=open("icons.txt", "w")
+
+for p in doc.getElementsByTagName('path'):
+    id, d = map(p.getAttribute, ('id','d'))
+    stroke = 'black' if id in ('yellow','white','pink','orange') else 'white';
+    print(f'.{id}::after {{ background: {id}; content:url(\'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path style="stroke-width: 25px; fill: none; stroke: {stroke};" d="{d}" /></svg>\') }}', file=f)
+
